35c7e54612f76a9254142f8d6719a875bfd516b1
Kasia902 <khrystyna.shumska@gmail.com>
2019-03-04 10:53:53 +0200

    made changes to method

--- elements/base.element.js
+++ elements/base.element.js
@@ -5,31 +5,26 @@ class BaseElement {
         this.pageObject = pageObject;
     }
 
-<<<<<<< HEAD
     getProtractorElement() {
         return this.protractorElement;
     }
 
-=======
->>>>>>> ec5cb32d5a0124684e163293a4fa0670eedc0c55
     async click() {
         console.log(`Click ${this.constructor.name} "${this.elementName}" on "${this.pageObject.constructor.name}"`);
         await this.protractorElement.click();
     }
-<<<<<<< HEAD
 
     async isDisplayed() {
-        console.log(`${this.constructor.name} "${this.elementName}" is visible on "${this.pageObject.constructor.name}"`);
-        await this.protractorElement.isDisplayed();
-        return true;
+        if (await this.protractorElement.isDisplayed()) {
+            console.log(`${this.constructor.name} "${this.elementName}" is visible on "${this.pageObject.constructor.name}"`);
+            return true;
+        }
     }
 
     async sendKeys() {
         console.log(`Enter value to ${this.constructor.name} "${this.elementName}" on "${this.pageObject.constructor.name}"`);
         await this.protractorElement.sendKeys();
     }
-=======
->>>>>>> ec5cb32d5a0124684e163293a4fa0670eedc0c55
 }
 
 module.exports = BaseElement;
