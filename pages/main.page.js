503b6f56ba7512c89d7a23935c538a85cf99b653
Kasia902 <khrystyna.shumska@gmail.com>
2019-03-05 15:29:45 +0200

    wotk on 5 test case. in progress

--- pages/main.page.js
+++ pages/main.page.js
@@ -1,9 +1,14 @@
 let BasePage = require("./base.page");
 let WebButton = require("../elements/button.element");
+let WebInput = require("../elements/webinput.element.js");
 
 let EC = protractor.ExpectedConditions;
 let loginButtonLocator = by.xpath('//*[@class="item-login"]');
 let userIconLocator = by.xpath('//*[@class="name ellipsis"]');
+let searchFieldLocator = by.xpath('//*[@id="searchbox"]');
+let submitSearchButtonLocator = by.xpath('//*[@id="doSearch"]');
+let cityBannerCloseLocator = by.xpath('//*[@class="close"]');
+
 
 class MainPage extends BasePage {
     // PO Actions
@@ -23,6 +28,27 @@ class MainPage extends BasePage {
             await this.getLoginButtonElement().click();
         })();
     }
+
+    async searchItem(searchText) {
+        await allure.createStep("Search for item", async () => {
+            await this.geSearchFieldElement().click();
+            await (this.geSearchFieldElement().getProtractorElement()).clear();
+            await this.geSearchFieldElement().sendKeys(searchText);
+            await this.getSubmitSearchButtonElement().click();
+        })();
+    }
+
+    async closeCityBanner() {
+        await allure.createStep("Close annoying city banner", async () => {
+            await this.getCityBannerCloseButtonElement().click();
+        })();
+    }
+
+    async clearForm() {
+        await (this.geSearchFieldElement().getProtractorElement()).clear();
+    }
+
+
     // PO Getters
 
     getLoginButtonElement() {
@@ -31,6 +57,18 @@ class MainPage extends BasePage {
 
     getUserIconElement() {
         return new WebButton(element(userIconLocator), "User Icon Button", this);
+    }
+
+    geSearchFieldElement() {
+        return new WebInput(element(searchFieldLocator), "Search Field", this);
+    }
+
+    getSubmitSearchButtonElement() {
+        return new WebButton(element(submitSearchButtonLocator), "Submit Search Button", this);
+    }
+
+    getCityBannerCloseButtonElement() {
+        return new WebButton(element(cityBannerCloseLocator), "Navigate Login Button", this);
     }
 }
 
